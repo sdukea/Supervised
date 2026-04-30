5ef6ad40175b2773798ef30c47d73fd4c268ef3c
Syon Duke Abraham <kordan.goat@gmail.com>
2026-04-30 13:35:04 +0530

    Compute gradient terms

--- gradient_descent.py
+++ gradient_descent.py
@@ -12,7 +12,7 @@ y_train = np.array([300.0, 500.0]) # for each value, a label
 
 def compute_cost(x, y, w, b):
 
-    m = x_train.shape[0]
+    m = x.shape[0]
 
     total_cost = 0
 
@@ -28,3 +28,28 @@ def compute_cost(x, y, w, b):
     return final_cost
 
 # returns final cost
+
+def compute_gradient_terms(x, y, w, b):
+    
+    m = x.shape[0]
+
+    # exactly what we're computing
+    dj_dw = 0 # gradient term for w update
+    dj_db = 0 # gradient term for b update
+    # start accumulating both these now to return it
+
+    for i in range(m):
+        
+        f_wb = w * x[i] + b
+        # model prediction - y-hat/estimate (as you know it)
+
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = (f_wb - y[i])
+
+        dj_dw += dj_dw_i
+        dj_db += dj_db_i
+    
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+# returns the gradient terms to implement g.d.
