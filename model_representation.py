f89e16a70598276c9d1458ae4d9a31eaa76d5c5e
Syon Duke Abraham <kordan.goat@gmail.com>
2026-04-30 12:43:29 +0530

    Compute model output

--- model_representation.py
+++ model_representation.py
@@ -46,3 +46,68 @@ print(f"Tr. eg.: {m}")
 
 # so, examples = m = x_train.shape[0] = first value
 
+# set x_i and y_i
+# has to be iterable to set i = 0 ready for iteration and then
+# index x_i indexed to i and y_i indexed to i, making both ready for iter.
+
+i = 0
+
+x_i = x_train[i]
+
+y_i = y_train[i]
+
+# plot
+
+plt.scatter(x_train, y_train, marker='x', c='r')
+
+plt.title('Housing prices')
+
+plt.ylabel('Price (1000s)')
+
+plt.xlabel('Size (1000 sq ft)')
+
+plt.show()
+
+# parameters
+
+# let's set, for now,
+
+w = 100
+
+b = 100
+
+print(f"w: {w}\nb: {b}")
+
+# compute f_wb
+
+# NOTE: x - the data set of input features - is just 1 feature and has two values/tr. eg./data points
+# so, x^(i)/x_i - a training example consisting of one value only - not a vector because
+# only one value - would be x-arrow-on-top-vector^(i)/x_i_vector instead
+
+# For one feature, you have one weight w - and for each feature value/tr. eg./data point, you use
+# the same w because it is only of one feature
+
+# for x_0, f_wb = w * x_0 + b (for first data point/tr. eg/feature value)
+# for x_1, f_wb = w * x_1 + b (for the second)
+
+# can't do it for all if there are more tr.eg/feature values
+
+def compute_model_output(x, w, b):
+    """
+    Computes prediciton of a linear model
+
+    returns f_wb: model prediction
+    """
+
+    m = x.shape[0]
+
+    f_wb = np.zeros(m)
+
+    # Create an array of m zeros i.e. for each tr. eg., you need a prediction
+    # m tr. eg = m predictions (initialize predictions to zero now)
+
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+    
+    return f_wb
+
