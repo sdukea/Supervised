cddbdccc5837cb7f70ae2062609731f798226a90
Syon Duke Abraham <kordan.goat@gmail.com>
2026-04-30 12:07:16 +0530

    All the required notations

---
+++ model_representation.py
@@ -0,0 +1,15 @@
+# Learning to implement the model f w,b for linear regression with one variable
+
+# Notations:
+
+# a, unbold - scalar
+# a, bold - vector (visible in PDF)
+# x - dataset of features
+# y - labels for each training example in x
+# x_train - part of x taken for training l.r. model
+# y_train - labels for the x_train taken part of x
+# x_i, y_i - ith training example
+# m - number of training examples
+# w, b - parameters
+# f_wb - result of the model evaluation at x_i parameterized by w, b
+
