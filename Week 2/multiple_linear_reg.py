eabe40e384ee8aa9a126f44c5e8a254489bdeaaa
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-03 17:40:50 +0530

    Set near optimal w (vector) and b

--- Week 2/multiple_linear_reg.py
+++ Week 2/multiple_linear_reg.py
@@ -37,8 +37,30 @@ f_w,b(x^(i)) → model prediction for i-th example
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 
+# features (in order): size, no: of bedrooms, no: of floors, age of home
+# target: price of house
+
 # X_train is now a matrix/2D array consisting of 1-D arrays as its values where each 1-D array
 # is a training example/row with 4 values -> hence, we have 4 features
 # for each training example, we have 1 target
 # 3 training examples, each with 4 values/4 features and we have 3 targets for each tr.eg in y_train
+
+# m = 3 (number of rows) - X_train.shape[0]
+# n = 4 (number of columns) - X_train.shape[1]
+
+print(f"X shape: {X_train.shape}")
+print(f"y shape: {y_train.shape}")
+
+# w is a vector (1D array) with n elements in it, for each feature
+
+# x^(i) is a vector, representing one traininge example, unlike our previous univariate linear 
+# reg. example where x^(i) was just a sinlge value as x_train (X_train here as it is a matrix now and
+# 'x' is capitalized to 'X') had only one feature
+
+# x^(i)j is a feature value from the x^(i) vector
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+
+# loading w and b that are already near optimal for demonstration 
 
