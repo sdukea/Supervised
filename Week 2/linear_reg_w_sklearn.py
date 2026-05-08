a7f994b0735024dd8d0e369f8782688dff706756
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-08 13:08:17 +0530

    Plot pred. vs targets

--- Week 2/linear_reg_w_sklearn.py
+++ Week 2/linear_reg_w_sklearn.py
@@ -108,7 +108,17 @@ y_pred_sgd = sgdr.predict(X_norm) # predictions using sgdr.predict
 
 # no test set; use tr. set again
 
-y_pred = np.dot(w_norm, X_norm) + b_norm # predictions using np.dot()
+y_pred = np.dot(X_norm, w_norm) + b_norm # predictions using np.dot()
 
 # both should match
+
+# plot predictions and targets vs original features    
+fig,ax=plt.subplots(1,4,figsize=(12,3),sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train, label = 'target')
+    ax[i].set_xlabel(X_features[i])
+    ax[i].scatter(X_train[:,i],y_pred,color='orange', label = 'predict')
+ax[0].set_ylabel("Price"); ax[0].legend();
+fig.suptitle("target versus prediction using z-score normalized model")
+plt.show()
 
