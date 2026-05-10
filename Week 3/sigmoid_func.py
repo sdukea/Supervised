023bbee5a66028b11f56c78385859d1c86a23fb1
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-11 00:24:20 +0530

    Implement sig. func.

--- Week 3/sigmoid_func.py
+++ Week 3/sigmoid_func.py
@@ -18,3 +18,69 @@ import matplotlib.pyplot as plt
 # so a number between 0 or 1 is the outcome we will need for classification tasks;
 # i.e. the probability
 
+# a l.r. model predicts a random number based on parameters and input values via the model
+# f_wb_x_i = w * x_i + b
+
+# but we only need a number – that is probabilistic of the outcome – between 0 or 1
+
+# and probabilities cannot be a number below 0 or above 1
+
+# so a sigmoid function does this:
+# 1. squishes every number into 0 to 1
+
+# specifically, it takes in any number
+# -1000, -500, 0, 8, 999... anything
+
+# and converts it into:
+
+# a number between 0 to 1 that is probabilistic of the outcome present in virtue of the two categories
+# we have (2 in our case; that is the most general case)
+
+# in classifc, the outcome/class/probabilistic value is from a finite set of categories (2 here - 0/
+# no/absent or 1/yes/present)
+
+# and in reg, we predict a value that is from a spectrum of values; infinite
+# after plotting the regression model, this plot, as you know before, can be zoomed in and out and
+# and go on forever infinitely
+# and also the model also extends forever in both directions so there exists an unbounded, continous
+# range FOREVER
+
+# and our prediction is one of that – not from a finite set of categories but from an infinite spectrum
+
+# that's the difference
+
+# formula for s. func.
+
+input_arr = np.array([1, 2, 3])
+
+exp_arr = np.exp(input_arr)
+
+# does e raised to all the elements in the given array 'input_arr'
+
+print(input_arr)
+print(exp_arr)
+
+# input is just a single number
+# because sigmoid functions operates on one value at a time
+
+# so, the func. takes one number in (any) and one number out (0 to 1)
+
+input_val = 1
+exp_val = np.exp(input_val)
+
+# implement.
+
+def sigmoid(z):
+    
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+
+    
