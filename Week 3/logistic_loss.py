f033763835418fb96a560a73bec11877480c07cb
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-14 11:58:58 +0530

    Load data

---
+++ Week 3/logistic_loss.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# see why sq. error loss function is not appropriate for logistic regression
+# exploring the logistic loss function
+
+# get data
+x_train = np.array([0, 1, 2, 3, 4, 5], dtype=np.longdouble)
+y_train = np.array([0,0,0,1,1,1])
+
