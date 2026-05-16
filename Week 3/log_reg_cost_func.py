f2c4a9fea5d97f69b3bb0da2f7af2ab04260fdee
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-16 23:41:18 +0530

    Get tr. data

---
+++ Week 3/log_reg_cost_func.py
@@ -0,0 +1,7 @@
+# examine implementation and utilize c.f. for logistic regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
+y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
