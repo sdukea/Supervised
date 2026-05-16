135cb771bc15eb0a349b44b57bee71db275d9629
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-16 23:42:28 +0530

    Create bool. mask

--- Week 3/log_reg_cost_func.py
+++ Week 3/log_reg_cost_func.py
@@ -5,3 +5,11 @@ import matplotlib.pyplot as plt
 
 X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
 y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
+
+# so two features; X_train is a 2D matrix
+
+# plot data
+
+pos = y_train == 1
+neg = y_train == 0
+
