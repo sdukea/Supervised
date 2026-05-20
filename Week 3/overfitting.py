f5a514d5ec8febf8b1dccef066d2be2d204f068b
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-20 23:46:23 +0530

    Crescent moons and why

--- Week 3/overfitting.py
+++ Week 3/overfitting.py
@@ -9,3 +9,23 @@ from ipywidgets import Output
 
 # let's DIY
 
+from sklearn.datasets import make_moons
+
+# we're demonstrating overfitting on a dataset that has a complex relationship
+# so data is like:
+# two crescent moons side by side where one is facing upward and another facing downward
+
+# so if we'd want to fit a model to this, we'd have to build a careful model (polynomial, of course)
+# where there are high chances of overfitting
+
+# that's why there's this example dataset
+# 1. to be able to fit a complex polyn. model to the dataset
+# 2. have high risk of overfitting so that we can see it and solve it
+
+
+
+X, y = make_moons(
+    n_samples=300,
+    noise=0.25,
+    random_state=42
+)
