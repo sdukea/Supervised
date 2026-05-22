597912c2923a73f70cf58dee9bd4cb96a0fc4820
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-22 23:32:23 +0530

    + intuition

--- Week 3/regularization.py
+++ Week 3/regularization.py
@@ -6,3 +6,13 @@ import matplotlib.pyplot as plt
 # to reduce overfitting, regularization or reduce the number/impact of features we have? 
 # Isn't that counter-intuitive?
 
+# the problem in overfitting:
+# - flexibility: how easily the model can bend itself to fit data
+# - a very flex. model can create extremely complicated decision boundaries or curves
+# More flexibility means:
+# better ability to capture complex real patterns
+# but also better ability to memorize noise
+# yeah, you also learn 'e' (epsilon) which is noise that cannot be predicted but exists no matter what
+
+# one way to think: overfitting = model complexity / amount of data
+
