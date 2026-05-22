5a8f0558d0b0c4779b577f10f4e27ff9f32a40c2
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-22 23:28:12 +0530

    Counter-intuition attack

--- Week 3/regularization.py
+++ Week 3/regularization.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# If one way to reduce overfitting is 
+# to add more training examples, then why is another way, 
+# to reduce overfitting, regularization or reduce the number/impact of features we have? 
+# Isn't that counter-intuitive?
+
